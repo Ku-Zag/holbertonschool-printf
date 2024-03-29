728e893eb4bdad86fff613e5da2a4ff846207e58
llammann <bd7n2v9tr@code.edu.az>
2024-03-29 13:04:11 +0000

    updated main.h

--- main.h
+++ main.h
@@ -1,4 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+
 int _printf(const char *format, ...);
+
+typedef struct type_s 
+{
+	char *t;
+	int (*f)(va_list);
+} func_t;
+
+int _write(char c);
+int print_str(va_list args);
+int print_char(va_list args);
+int print_fs(va_list args);
 #endif 
