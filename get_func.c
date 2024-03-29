ae6af689a54c475db2b2a4774c0341b56548144d
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-29 13:05:14 +0000

    added get_func.c

---
+++ get_func.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * get_func - it picks up which function to go
+ * @format: an input char
+ * Description: format
+ * Return: pointer to func
+ */
+int (*get_func(const char *format))(va_list)
+{
+	int i = 0;
+	func_t list[] = {
+		{"%", print_fs},
+		{"c", print_char},
+		{"s", print_str},
+		{NULL, NULL}
+	};
+
+	while(list[i].t)
+	{
+		if (*(list[i].t) == *format)
+			return (list[i].f);
+		i++;
+	}
+	returnn (NULL);
+}
